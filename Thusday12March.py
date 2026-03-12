7448375f5a30254ceb50656b20e06a58377d4faf
Dilapidated_G3 <grantsamson139@gmail.com>
2026-03-12 12:10:59 +0200

    Update Thusday12March.py
    Implemented a simple login simulation with default credentials.

--- Thusday12March.py
+++ Thusday12March.py
@@ -1,1 +1,24 @@
 #Code here
+# Created by Grant Samson
+#Creating a python program that stimulates logging into a system. Set the defualt username and password to:
+#username = "admin"
+#password = "admin@2026"
+
+username = "admin"
+password = "admin@2026"
+
+user=input("Input your username ")
+passw=input("input your password ")
+
+invalid=("password is incorrect")
+incorrect=("username is incorrect")
+
+if username == user and password == passw:
+    print("You have successfully logged in")
+elif ( username != user and password != passw):
+    print(invalid)
+    print(incorrect)
+elif (username == user and password != passw):
+    print(incorrect)
+else:
+    print(invalid)
