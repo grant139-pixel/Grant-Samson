7ba1ab79d02cdda27b728afd28ed6d363de83b42
Dilapidated_G3 <grantsamson139@gmail.com>
2026-03-12 20:27:46 +0200

    Fix comments and spelling in login simulation
    Corrected spelling and improved comments in the code.

--- Thusday12March.py
+++ Thusday12March.py
@@ -1,18 +1,21 @@
 #Code here
 # Created by Grant Samson
-#Creating a python program that stimulates logging into a system. Set the defualt username and password to:
+#Creating a Python program that stimulates logging into a system. Set the default username and password to:
 #username = "admin"
 #password = "admin@2026"
 
 username = "admin"
 password = "admin@2026"
 
+#setting keywords for what users should do when the specific things are displayed
+
 user=input("Input your username ")
 passw=input("input your password ")
+# Here I set commands for what should be displayed when the password or username entered by the individual is incorrect
 
 invalid=("password is incorrect")
 incorrect=("username is incorrect")
-
+# Set my commands for what should happen when the username and password match, and when it doesn't match with the default username and password, what should be displayed if it is incorrect
 if username == user and password == passw:
     print("You have successfully logged in")
 elif ( username != user and password != passw):
